12b957d2418443e265fe0bb859746385f520be43
221V <221v92@gmail.com>
2018-10-28 21:56:38 +0200

    fix warning in erl v20+

--- src/rest_sup.erl
+++ src/rest_sup.erl
@@ -1,7 +1,7 @@
 -module(rest_sup).
 -behaviour(supervisor).
 -export([start_link/0, init/1]).
--compile(export_all).
+-compile([export_all, nowarn_export_all]).
 
 start_link() -> supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 init([]) -> {ok, {{one_for_one, 5, 10}, []}}.
